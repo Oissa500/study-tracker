a75f948010a52d0b8a40a955484c71f62863a135
Omar Issa <omarissa4988@gmail.com>
2026-05-13 16:18:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ import os
 
 app = Flask(__name__)
 db = SQL(os.getenv("DATABASE_URL", "sqlite:///study.db"))
+if not os.path.exists("study.db"):
+    open("study.db", "w").close()
+
+    with open("schema.sql") as f:
+        sql_script = f.read()
+
+    for statement in sql_script.split(";"):
+        if statement.strip():
+            db.execute(statement)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
